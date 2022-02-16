9ac6c5e0871a7dde398d3d4ff12e819e0fc1797a
LunarDeluged <lilliebohr@gmail.com>
2022-02-15 22:18:24 -0600

    quiz 2
    new

--- Quiz.java
+++ Quiz.java
@@ -1,36 +1,39 @@
 import javax.swing.JOptionPane;
 public class Quiz {
-
-	public static void main(String[] args) {
-		String question = "What planet do we live on? \n";
-		question += "A. Mars \n";
-		question += "B. Jupiter \n";
-		question += "C. Neptune \n";
-		question += "D. Mercury \n";
-		question += "E. Earth \n";
-		while(true) {
+	static int nQuestions = 0;
+	static int nCorrect = 0;
+	
+	static String ask(String question) {
+		question += "A. 8 \n";
+		question += "B. 166 \n";
+		question += "C. 9765625 \n";
+		question += "D. 81 \n";
+		question += "E. 45 \n";
 		String answer = JOptionPane.showInputDialog(question);
 		answer = answer.toUpperCase();
+		return answer;	
+	}
+	
+	static void check(String question, String correctAnswer) {
+		String answer = ask(question);
+		if (answer.equals(correctAnswer)){
+			JOptionPane.showMessageDialog(null,"Correct!");	
+			nCorrect++;
+		}
+		else if (answer.equals("A") || answer.equals("B") || answer.equals("C") || answer.equals("D") || answer.equals("E") ) {
+			JOptionPane.showMessageDialog(null,"Incorrect, the correct answer was " + correctAnswer);
+		}
+		else JOptionPane.showMessageDialog(null,"Invalid Input please enter A, B, C, D, or E");
+		nQuestions++;
+		}
+	
+	public static void main(String[] args) {
+		check("What is 2 * 4? \n", "A");
+		check("What is 5 to the power of 10? \n", "C");
+		check("What is 9 squared? \n", "D");
+		check("What is 78 + 88? \n","B");
 		
-		if (answer.equals("E")) {
-			JOptionPane.showMessageDialog(null,"Correct!");
-			break;
-		}
-		else if (answer.equals("A")) {
-			JOptionPane.showMessageDialog(null,"Incorrect!");
-		}
-		else if (answer.equals("B")) {
-			JOptionPane.showMessageDialog(null,"Incorrect!");
-		}
-		else if (answer.equals("C")) {
-			JOptionPane.showMessageDialog(null,"Incorrect!");
-		}
-		else if (answer.equals("D")) {
-			JOptionPane.showMessageDialog(null,"Incorrect!");
-		}
-		else JOptionPane.showMessageDialog(null,"Invalid Input please try again");
-		}
+		JOptionPane.showMessageDialog(null, nCorrect + " out of " + nQuestions + " questions correct");
 	}
 }
-
 
